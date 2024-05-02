c750c16cc5defcc3edf2d28c436951a4a1d6f143
Simon Saez <nayxxaxx789super@gmail.com>
2024-05-01 22:34:28 -0400

    [Update] OC Proveedor
    Se añadió un constructor

--- ordenes-de-compra/src/main/java/mingetal/MCM/ordenesdecompra/entity/OrdenesDeCompraProveedorEntity.java
+++ ordenes-de-compra/src/main/java/mingetal/MCM/ordenesdecompra/entity/OrdenesDeCompraProveedorEntity.java
@@ -23,4 +23,12 @@ public class OrdenesDeCompraProveedorEntity {
     private LocalDate fecha_pago; // yyyy-mm-dd
     private LocalDate fecha_entrega; //yyyy-mm-dd
     private String factura;
+
+    public OrdenesDeCompraProveedorEntity(int id_proveedor, String estado_pago, LocalDate fecha_pago, LocalDate fecha_entrega, String factura) {
+        this.id_proveedor = id_proveedor;
+        this.estado_pago = estado_pago;
+        this.fecha_pago = fecha_pago;
+        this.fecha_entrega = fecha_entrega;
+        this.factura = factura;
+    }
 }
