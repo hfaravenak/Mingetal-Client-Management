0ec038067222a42e7249e30085dab0130c194d99
Simon Saez <nayxxaxx789super@gmail.com>
2024-05-14 01:35:38 -0400

    [Update] Cliente
    - Muestra la lista en orden de Alfabeto
    - Cuando registra un cliente, hace que la primera letra de cada palabra tenga una mayuscula en el nombre

--- cliente/src/main/java/mingetal/MCM/cliente/Services/ClienteService.java
+++ cliente/src/main/java/mingetal/MCM/cliente/Services/ClienteService.java
@@ -13,9 +13,23 @@ public class ClienteService {
     @Autowired
     ClienteRepository clienteRepository;
 
-    public void save(ClienteEntity clienteEntity) { clienteRepository.save(clienteEntity); }
+    public void save(ClienteEntity clienteEntity) {
+        String[] palabras = clienteEntity.getNombre().split("\\s+");
+        StringBuilder sb = new StringBuilder();
 
-    public List<ClienteEntity> findAll() { return clienteRepository.findAll(); }
+        for (String palabra : palabras) {
+            // Convertir la primera letra de la palabra a mayúscula y agregar el resto de la palabra
+            sb.append(Character.toUpperCase(palabra.charAt(0))).append(palabra.substring(1)).append(" ");
+        }
+        clienteEntity.setNombre(sb.toString().trim());
+        clienteRepository.save(clienteEntity);
+    }
+
+    public List<ClienteEntity> findAll() {
+        List<ClienteEntity> clienteEntities = clienteRepository.findAll();
+        clienteEntities.sort((p1, p2) -> p1.getNombre().compareTo(p2.getNombre()));
+        return clienteEntities;
+    }
 
     public ClienteEntity findByRut(String rut) { return clienteRepository.findByRut(rut); }
 
@@ -27,10 +41,17 @@ public class ClienteService {
                 resultados.add(nombreDeLista);
             }
         }
+
+        resultados.sort((p1, p2) -> p1.getNombre().compareTo(p2.getNombre()));
+
         return resultados;
     }
 
-    public List<ClienteEntity> findByEmpresa(String empresa) { return clienteRepository.findByEmpresa(empresa); }
+    public List<ClienteEntity> findByEmpresa(String empresa) {
+        List<ClienteEntity> clienteEntities = clienteRepository.findByEmpresa(empresa);
+        clienteEntities.sort((p1, p2) -> p1.getNombre().compareTo(p2.getNombre()));
+        return clienteEntities;
+    }
 
     public ClienteEntity delete(String rut) {
         ClienteEntity clienteEntity = findByRut(rut);
