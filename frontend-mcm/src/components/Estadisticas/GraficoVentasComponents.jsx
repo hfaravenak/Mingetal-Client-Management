4bc39b3f4e8e3266baea432892c65391dd54e9ce
Felipe Aravena <hernan.aravena.k@usach.cl>
2024-07-05 01:56:54 -0400

    [Fix] Se modifica el width
    De los contenedores de algunos grafcos.

--- frontend-mcm/src/components/Estadisticas/GraficoVentasComponents.jsx
+++ frontend-mcm/src/components/Estadisticas/GraficoVentasComponents.jsx
@@ -119,7 +119,7 @@ const NavStyle = styled.nav`
     padding: 2%;
     border: 2px solid #D5D5D5;
     background-color: #f0f0f0;
-    width: 80%;
+    width: auto;
 }
 .charts {
     display: flex;
