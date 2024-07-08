a7ea365717002849939dfa95fd4d54ad62f02d07
Felipe Aravena <hernan.aravena.k@usach.cl>
2024-07-08 01:26:36 -0400

    [Feature] Login y Register en Backend
    Ya se pueden realizar estas funcionalidades.

--- Login/src/main/resources/import.sql
+++ Login/src/main/resources/import.sql
@@ -1,2 +1,3 @@
--- TABLA LOGIN DE USUARIOS
-insert into login (id, nombres, password, correo, intentos, bloqueado) values (1, 'Waldo Santibañez', 'lol', 'waldo@mingetal.cl', 0, false);
+-- TABLA USUARIO PARA LOGIN
+insert into usuario (nombres, password, correo, intentos, bloqueado) values ('Waldo Santibañez', '$2a$10$eWIxfQFarHUMyEiIHXHmsersS98MnoXUzmgptVOAqLYzr9/tdC9H.', 'waldo@mingetal.cl', 0, false);
+--La contraseña desencriptada es: waldo123
